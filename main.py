1214d694639f8ea6b1c82b5a4e80cfbd7b897b71
bence <bence.matuz1@gmail.com>
2025-02-20 11:21:42 +0100

    feat: handle special chars

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-new_alphabet = []
 
 direction = input("Type 'encode' to encrypt, \
 type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#def ceaser(original_text, shift_amount, decode_or_encode):
-
 
 def encrypt(original_text, shift_amount):
-    encrypted_word = ""
+    encrypted_text = ""
     for letter in original_text:
-        index = alphabet.index(letter)
-        shifted_index = index + shift_amount
-        if shifted_index >= len(alphabet):
-            encrypted_word += alphabet[shifted_index - len(alphabet)]
+        if letter not in alphabet:
+            encrypted_text += letter
         else:
-            encrypted_word += alphabet[shifted_index]
-    print(encrypted_word)
+            index = alphabet.index(letter)
+            shifted_index = index + shift_amount
+            if shifted_index >= len(alphabet):
+                encrypted_text += alphabet[shifted_index - len(alphabet)]
+            else:
+                encrypted_text += alphabet[shifted_index]
+    return encrypted_text
 
 
 def decrypt(original_text, shift_amount):
     decrypted_text = ""
     for letter in original_text:
-        index = alphabet.index(letter)
-        decrypted_text += alphabet[index - shift_amount]
-    print(decrypted_text)
+        if letter not in alphabet:
+            decrypted_text += letter
+        else:
+            index = alphabet.index(letter)
+            decrypted_text += alphabet[index - shift_amount]
+    return decrypted_text
 
 
 def ceaser(decrypt_or_encrypt, original_text, shift_amount):
+    result = ""
     if decrypt_or_encrypt == "encode":
-        encrypt(original_text, shift_amount)
+        result = encrypt(original_text, shift_amount)
+        print(f"Your result is: {result}")
     elif decrypt_or_encrypt == "decode":
-        decrypt(original_text, shift_amount)
+        result = decrypt(original_text, shift_amount)
+        print(f"Your result is: {result}")
     else:
         print("Wrong input!")
 
