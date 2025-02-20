4d7bba1c642a51253af2140f604cc56cfb3064d0
bence <bence.matuz1@gmail.com>
2025-02-20 11:29:41 +0100

    feat: add logo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
 direction = input("Type 'encode' to encrypt, \
 type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
